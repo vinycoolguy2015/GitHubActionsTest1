6835e1014036d1e7c17c62086fe10b51efa467ba
vinycoolguy2015 <vinayakpandeyit@gmail.com>
2019-12-09 13:36:27 +0530

    Update bootstrap.tf

--- modules/bootstrap/bootstrap.tf
+++ modules/bootstrap/bootstrap.tf
@@ -30,24 +30,3 @@ resource "aws_s3_bucket" "state_bucket" {
   }
 }
 
-# Build a DynamoDB to use for terraform state locking
-resource "aws_dynamodb_table" "tf_lock_state" {
-  name = var.dynamo_db_table_name
-
-  # Pay per request is cheaper for low-i/o applications, like our TF lock state
-  billing_mode = "PAY_PER_REQUEST"
-
-  # Hash key is required, and must be an attribute
-  hash_key = "LockID"
-
-  # Attribute LockID is required for TF to use this table for lock state
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-
-  tags = {
-    Name    = var.dynamo_db_table_name
-    BuiltBy = "Terraform"
-  }
-}
