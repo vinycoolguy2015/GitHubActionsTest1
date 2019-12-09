725bdbbe70dd123fff67270c1f8addcc83a10e05
vinycoolguy2015 <vinayakpandeyit@gmail.com>
2019-12-09 13:09:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ provider "aws" {
 
 # Build the VPC
 resource "aws_vpc" "vpc" {
-  cidr_block           = "10.2.0.0/16"
+  cidr_block           = "10.3.0.0/16"
   instance_tenancy     = "default"
 
   tags = {
